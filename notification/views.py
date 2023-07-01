c65b54db343973fecbdcd77e5a0a524f5083605a
Mohamed Alaa <ma722072207220@gmail.com>
2023-07-02 00:17:08 +0300

    updated

--- notification/views.py
+++ notification/views.py
@@ -60,7 +60,7 @@ class AllNotificationPatient(views.APIView):
 class NotificationHeadNursing(views.APIView):
     def get(self, request):
         notification = NotificationApp.objects.select_related(
-            'user_sender', 'patient').prefetch_related('user_receiver').all()
+            'user_sender', 'patient').prefetch_related('user_receiver').filter(status=False)
         serializer = NotificationHeadNursingSerializer(
             notification, many=True).data
         return Response({'result': notification.count(), "data": serializer}, status=status.HTTP_200_OK)
